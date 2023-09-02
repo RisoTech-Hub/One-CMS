de1a63b2aba221021b8ed4db3fdf72cec986a9ce
Bin Nguyễn <tu.nguyen@risotech.vn>
2023-09-02 13:39:24 +0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+from distutils.util import convert_path
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+main_ns = {}
+ver_path = convert_path("cms/__init__.py")
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+with open("README.md") as readme_file:
+    long_description = readme_file.read()
+
+setup(
+    name="riso-cms",
+    version=main_ns["__version__"],
+    description="CMS base structure extend Django Flatpages with GrapeJs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Bin Nguyen",
+    author_email="tu.nguyen@risotech.vn",
+    url="https://github.com/RisoTech-Hub/one-cms",
+    packages=["cms"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment ::  Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 4.2",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Application Frameworks",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    include_package_data=True,
+    python_requires=">=3.11",
+    install_requires=[
+        "Django==4.2",
+    ],
+)
